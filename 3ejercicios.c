ba3bc87ff8e64089f4528408e2ad8017d1d23502
Alejandro Crespo <alejandro.crespo@udla.edu.ec>
2026-05-15 14:12:15 -0500

    definir puntero

--- 3ejercicios.c
+++ 3ejercicios.c
@@ -2,6 +2,13 @@
 
 int main (int argc, char*argv[]){
     int arreglo[]={3,6,9,12,15,18};
+    
+    int*ptr;
+    ptr= arreglo;
+    printf("%d %d", ptr, *ptr);
+    
+    
+    
     for (int i = 0; i<6; i++)
     {
         printf("%d", arreglo[i]);
